4c12a0832a099752062e9466be9391d2f02cb4d2
Christian Fobel <christian@fobel.net>
2013-02-09 18:02:32 -0500

    Clean up test example

--- tests/test_socket_group_device.py
+++ tests/test_socket_group_device.py
@@ -1,29 +1,55 @@
 from datetime import datetime
 
-from socket_group import ProcessSocketGroupDevice, DeferredSocket, PeriodicCallback
+import zmq
+
+from socket_group import ProcessSocketGroupDevice, DeferredSocket
 
 
-if __name__ == '__main__':
-    from pprint import pprint
-    import zmq
+def get_server():
+    # Configure server
+    #    The server simply echoes any message received, by sending the same
+    #    message back as a response.
+    def echo(self, multipart_message):
+        self.socks['rep'].send_multipart(multipart_message)
 
-    s = ProcessSocketGroupDevice()
+    server = ProcessSocketGroupDevice()
+    server.set_sock('rep',
+        DeferredSocket(zmq.REP)
+               .bind('ipc://ECHO:1')
+               .stream_callback('on_recv', echo)
+    )
+    return server
+
+
+def get_client():
+    # Configure client -----------------
+    #    The client simply sends a request every half second.  Each response
+    #    received is then dumped to `stdout`.
+    #    Each request contains a time-stamp and the words 'hello world'
+    client = ProcessSocketGroupDevice()
 
     def ping(self):
         self.socks['req'].send('[%s] hello world' % datetime.now())
 
     def pong(self, multipart_message):
-        print 'PONG:', multipart_message[0], self.socks['req']
+        print 'PONG:', multipart_message[0]
 
-    s.set_sock('req',
+    client.set_sock('req',
         DeferredSocket(zmq.REQ)
                .connect('ipc://ECHO:1')
                .stream_callback('on_recv', pong)
     )
-    s.set_periodic_callback('ping', (ping, 500))
+    client.set_periodic_callback('ping', (ping, 500))
+    return client
 
+
+if __name__ == '__main__':
+    server = get_server()
+    client = get_client()
     try:
-        s.start()
-        s.join()
+        server.start()
+        client.start()
+        client.join()
     except KeyboardInterrupt:
-        s.launcher.terminate()
+        client.launcher.terminate()
+    server.launcher.terminate()
