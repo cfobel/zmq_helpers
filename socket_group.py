bfaa2eee980dd35c825ae8b2bf72ae0dd75dd0cc
Christian Fobel <christian@fobel.net>
2013-02-10 03:12:45 -0500

    Refactor consumer processes into classes

--- socket_group.py
+++ socket_group.py
@@ -47,177 +47,9 @@ class DeferredSocket(object):
         return self
 
 
-class DeferredSocketGroup(object):
-    def __init__(self, deferred_socks=None):
-        if deferred_socks is None:
-            self._deferred_socks = OrderedDict()
-        else:
-            self._deferred_socks = OrderedDict(deferred_socks.items())
-
-    def set_sock(self, label, deferred_sock):
-        self._deferred_socks[label] = deferred_sock
-
-    def create_sockets(self, ctx):
-        socks = OrderedDict()
-
-        for label, deferred_sock in self._deferred_socks.iteritems():
-            socks[label] = zmq.Socket(ctx, deferred_sock.sock_type)
-
-        # set sockopts (must be done first, in case of zmq.IDENTITY)
-        for label, s in self._deferred_socks.iteritems():
-            for opt, value in s.sockopts:
-                socks[label].setsockopt(opt, value)
-
-        for label, s in self._deferred_socks.iteritems():
-            for bind_uri in s.binds:
-                socks[label].bind(bind_uri)
-
-        for label, s in self._deferred_socks.iteritems():
-            for connect_uri in s.connects:
-                socks[label].connect(connect_uri)
-        return socks
-
-    def create_streams(self, socks, io_loop):
-        streams = OrderedDict()
-        logging.debug('[create_streams] socks = %s' % socks)
-        for label, s in self._deferred_socks.iteritems():
-            if label in socks and s.stream_callbacks:
-                sock = socks[label]
-                stream = ZMQStream(sock, io_loop)
-                for stream_event, callback in s.stream_callbacks:
-                    logging.debug('[create_streams] %s %s %s %s' % (
-                            label, stream, stream_event, callback))
-                    # Register callback for stream event
-                    #   e.g., stream_event='on_recv'
-                    f = getattr(stream, stream_event)
-
-                    # Prepend `self` to `args` list for `callback`, to allow
-                    # callback targets to use the context of the containing
-                    # group, i.e., access the created sockets, etc.  This is
-                    # particularly useful when running the IOLoop in a thread
-                    # or process.
-                    f(lambda *args, **kwargs: callback(self, *args, **kwargs))
-                streams[label] = stream
-        return streams
-
-
-class SocketGroupDevice(DeferredSocketGroup):
-    """
-    A Threadsafe set of 0MQ sockets.
-    """
-    context_factory = zmq.Context.instance
-
-    def __init__(self, *args, **kwargs):
-        periodic_callbacks = kwargs.pop('periodic_callbacks', None)
-        if periodic_callbacks is None:
-            self._periodic_callbacks = OrderedDict()
-        else:
-            self._periodic_callbacks = OrderedDict(periodic_callbacks.items())
-        super(SocketGroupDevice, self).__init__(*args, **kwargs)
-
-    def set_periodic_callback(self, label, callback_defn):
-        assert(isinstance(callback_defn, tuple) and len(callback_defn) == 2)
-        self._periodic_callbacks[label] = callback_defn
-        return self
-
-    def _setup_periodic_callbacks(self, io_loop):
-        return self.create_periodic_callbacks(self._periodic_callbacks, io_loop)
-
-    def create_periodic_callbacks(self, callback_defns, io_loop):
-        periodic_callbacks = OrderedDict()
-        for label, (callback, period_ms) in callback_defns.items():
-            wrapped = functools.partial(callback, self)
-            periodic_callback = PeriodicCallback(
-                    lambda *args, **kwargs: wrapped(*args, **kwargs),
-                    period_ms, io_loop)
-            periodic_callbacks[label] = periodic_callback
-        for callback in periodic_callbacks.values():
-            callback.start()
-        return periodic_callbacks
-
-    def _setup_streams(self, socks, io_loop):
-        streams = self.create_streams(socks, io_loop)
-        return streams
-
-    def _setup_sockets(self, ctx):
-        socks = self.create_sockets(ctx)
-        return socks
-
-    def _setup_loop(self):
-        io_loop = IOLoop()
-        return io_loop
-
-    def _setup_loop_after_streams(self):
-        pass
-
+class SockConfigsTask(object):
     def run(self):
-        """The runner method.
-
-        Do not call me directly, instead call ``self.start()``, just like a
-        Thread.
-        """
-        self.context = self.context_factory()
-        self.socks = self._setup_sockets(self.context)
-        self.io_loop = self._setup_loop()
-        self.periodic_callbacks = self._setup_periodic_callbacks(self.io_loop)
-        self.streams = self._setup_streams(self.socks, self.io_loop)
-        #self._setup_loop_after_streams()
-
-        try:
-            self.io_loop.start()
-        except KeyboardInterrupt:
-            pass
-        self.done = True
-
-    def start(self):
-        """Start the device. Override me in subclass for other launchers."""
-        return self.run()
-
-    def join(self,timeout=None):
-        """wait for me to finish, like Thread.join.
-
-        Reimplemented appropriately by sublcasses."""
-        tic = time.time()
-        toc = tic
-        while not self.done and not (timeout is not None and toc-tic > timeout):
-            time.sleep(.001)
-            toc = time.time()
-
-
-class BackgroundSocketGroupDevice(SocketGroupDevice):
-    """Base class for launching SocketGroupDevices in background processes and threads."""
-
-    launcher=None
-    _launch_class=None
-
-    def __init__(self, *args, **kwargs):
-        super(BackgroundSocketGroupDevice, self).__init__(*args, **kwargs)
-        self.daemon = True
-
-    def start(self):
-        self.launcher = self._launch_class(target=self.run)
-        self.launcher.daemon = self.daemon
-        return self.launcher.start()
-
-    def join(self, timeout=None):
-        return self.launcher.join(timeout=timeout)
-
-
-class ThreadSocketGroupDevice(BackgroundSocketGroupDevice):
-    """A SocketGroup that will be run in a background Thread.
-
-    See SocketGroup for details.
-    """
-    _launch_class=Thread
-
-
-class ProcessSocketGroupDevice(BackgroundSocketGroupDevice):
-    """A SocketGroup that will be run in a background Process.
-
-    See SocketGroup for details.
-    """
-    _launch_class=Process
-    context_factory = zmq.Context
+        run_sock_configs(self.sock_configs)
 
 
 def create_sockets(ctx, deferred_socks):
@@ -288,14 +120,19 @@ def echo(socks, streams, multipart_message, delay=0):
     socks['rep'].send_multipart(multipart_message)
 
 
+class EchoServer(SockConfigsTask):
+    def __init__(self, bind_uri):
+        # Configure server
+        #    The server simply echoes any message received, by sending the same
+        #    message back as a response.
+        wrapped = functools.partial(echo, delay=0.3)
+        self.sock_configs = OrderedDict([
+                ('rep', DeferredSocket(zmq.REP)
+                            .bind(bind_uri)
+                            .stream_callback('on_recv', wrapped))
+        ])
+
+
 def run_echo_server(bind_uri):
-    # Configure server
-    #    The server simply echoes any message received, by sending the same
-    #    message back as a response.
-    wrapped = functools.partial(echo, delay=0.3)
-    sock_configs = OrderedDict([
-            ('rep', DeferredSocket(zmq.REP)
-                        .bind(bind_uri)
-                        .stream_callback('on_recv', wrapped))
-    ])
-    run_sock_configs(sock_configs)
+    echo_server = EchoServer(bind_uri)
+    echo_server.run()
--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -7,58 +7,78 @@ from collections import OrderedDict
 
 import zmq
 
-from socket_group import DeferredSocket, run_echo_server, run_sock_configs,\
-        get_run_context, PeriodicCallback, IOLoop, create_sockets_and_streams
+from socket_group import DeferredSocket, EchoServer, run_sock_configs,\
+        get_run_context, PeriodicCallback, SockConfigsTask
 
 
-def run_consumer(req_uri, push_uri, pull_uri, delay=0, push_bind=True,
+class Consumer(SockConfigsTask):
+    def __init__(self, req_uri, push_uri, pull_uri, delay=0, push_bind=True,
                  pull_bind=True):
-    logging.debug('[run_consumer] %s' % dict(
-        req_uri=req_uri,
-        push_uri=push_uri,
-        pull_uri=pull_uri,
-        delay=delay,
-        push_bind=push_bind,
-        pull_bind=pull_bind
-    ))
-    def process_input(socks, streams, stream, multipart_message):
+        self.delay = delay
+        self.uris = OrderedDict([
+                ('req', req_uri),
+                ('push', push_uri),
+                ('pull', pull_uri),
+        ])
+        self.sock_configs = OrderedDict([
+                ('req', DeferredSocket(zmq.REQ).connect(req_uri)),
+                ('push', DeferredSocket(zmq.PUSH)),
+                ('pull', DeferredSocket(zmq.PULL)
+                            .stream_callback('on_recv_stream', self.process_input)),
+        ])
+        if push_bind:
+            self.sock_configs['push'].bind(push_uri)
+        else:
+            self.sock_configs['push'].connect(push_uri)
+        if pull_bind:
+            self.sock_configs['pull'].bind(pull_uri)
+        else:
+            self.sock_configs['pull'].connect(pull_uri)
+
+    def process_input(self, socks, streams, stream, multipart_message):
         logging.debug('[CONSUMER:process_input] %s %s' % (stream,
                                                           multipart_message, ))
         async_id = multipart_message[0]
         message = multipart_message[1:]
         socks['req'].send_multipart(message)
         response = socks['req'].recv_multipart()
-        if delay > 0:
-            time.sleep(delay)
+        if self.delay > 0:
+            time.sleep(self.delay)
         socks['push'].send_multipart([async_id] + response)
         logging.debug('  \--> req response: %s' % response)
 
-    sock_configs = OrderedDict([
-            ('pull', DeferredSocket(zmq.PULL)
-                        .stream_callback('on_recv_stream', process_input)),
+
+class Producer(SockConfigsTask):
+    def __init__(self, rep_uri, pub_uri, push_uri, pull_uri, push_bind=False,
+                 pull_bind=False):
+        self.sock_configs = OrderedDict([
             ('push', DeferredSocket(zmq.PUSH)),
-            ('req', DeferredSocket(zmq.REQ).connect(req_uri))
-    ])
-    if push_bind:
-        sock_configs['push'].bind(push_uri)
-    else:
-        sock_configs['push'].connect(push_uri)
-    if pull_bind:
-        sock_configs['pull'].bind(pull_uri)
-    else:
-        sock_configs['pull'].connect(pull_uri)
-    run_sock_configs(sock_configs)
+            ('pull', DeferredSocket(zmq.PULL)
+                        .stream_callback('on_recv_stream', self.process_response)
+            ),
+            ('rep', DeferredSocket(zmq.REP)
+                        .bind(rep_uri)
+                        .stream_callback('on_recv_stream', self.process_request)
+            ),
+            ('pub', DeferredSocket(zmq.PUB).bind(pub_uri))
+        ])
 
+        if push_bind:
+            self.sock_configs['push'].bind(push_uri)
+        else:
+            self.sock_configs['push'].connect(push_uri)
+        if pull_bind:
+            self.sock_configs['pull'].bind(pull_uri)
+        else:
+            self.sock_configs['pull'].connect(pull_uri)
 
-def run_producer(rep_uri, pub_uri, push_uri, pull_uri, push_bind=False,
-        pull_bind=False):
-    def process_response(socks, streams, stream, multipart_message):
+    def process_response(self, socks, streams, stream, multipart_message):
         logging.debug('[PRODUCER:process_response] %s %s' % (stream,
                                                              multipart_message,
                                                              ))
         socks['pub'].send_multipart(multipart_message)
 
-    def process_request(socks, streams, stream, multipart_message):
+    def process_request(self, socks, streams, stream, multipart_message):
         logging.debug('[PRODUCER:process_request] %s %s' % (stream,
                                                             multipart_message,
                                                             ))
@@ -66,74 +86,56 @@ def run_producer(rep_uri, pub_uri, push_uri, pull_uri, push_bind=False,
         socks['rep'].send_multipart([async_id] + multipart_message)
         socks['push'].send_multipart([async_id] + multipart_message)
 
-    sock_configs = OrderedDict([
-        ('push', DeferredSocket(zmq.PUSH)),
-        ('pull', DeferredSocket(zmq.PULL)
-                    .stream_callback('on_recv_stream', process_response)
-        ),
-        ('rep', DeferredSocket(zmq.REP)
-                    .bind(rep_uri)
-                    .stream_callback('on_recv_stream', process_request)
-        ),
-        ('pub', DeferredSocket(zmq.PUB).bind(pub_uri))
-    ])
 
-    if push_bind:
-        sock_configs['push'].bind(push_uri)
-    else:
-        sock_configs['push'].connect(push_uri)
-    if pull_bind:
-        sock_configs['pull'].bind(pull_uri)
-    else:
-        sock_configs['pull'].connect(pull_uri)
+class Client(object):
+    def __init__(self, req_uri, sub_uri):
+        self.sock_configs = OrderedDict([
+                ('req', DeferredSocket(zmq.REQ).connect(req_uri)),
+                ('sub', DeferredSocket(zmq.SUB)
+                            .connect(sub_uri)
+                            .setsockopt(zmq.SUBSCRIBE, '')
+                            .stream_callback('on_recv_stream',
+                                             self.process_response))
+        ])
 
-    run_sock_configs(sock_configs)
-
-
-def run_client(req_uri, sub_uri):
-    def process_response(socks, streams, stream, multipart_message):
+    def process_response(self, socks, streams, stream, multipart_message):
         logging.debug('[CLIENT:process_response] %s %s' % (stream,
                                                              multipart_message,
                                                              ))
 
-    def do_request():
-        socks['req'].send_multipart(['hello world'])
-        response = socks['req'].recv_multipart()
+    def do_request(self):
+        self.socks['req'].send_multipart(['hello world'])
+        response = self.socks['req'].recv_multipart()
         logging.debug('[CLIENT:do_request] %s' % response)
 
-    sock_configs = OrderedDict([
-            ('req', DeferredSocket(zmq.REQ).connect(req_uri)),
-            ('sub', DeferredSocket(zmq.SUB)
-                        .connect(sub_uri)
-                        .setsockopt(zmq.SUBSCRIBE, '')
-                        .stream_callback('on_recv_stream', process_response))
-    ])
+    def run(self):
+        ctx, io_loop, self.socks, streams = get_run_context(self.sock_configs)
+        periodic_callback = PeriodicCallback(self.do_request, 1000, io_loop)
 
-    ctx, io_loop, socks, streams = get_run_context(sock_configs)
-    periodic_callback = PeriodicCallback(do_request, 1000, io_loop)
-
-    try:
-        periodic_callback.start()
-        io_loop.start()
-    except KeyboardInterrupt:
-        pass
+        try:
+            periodic_callback.start()
+            io_loop.start()
+        except KeyboardInterrupt:
+            pass
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
-    echo_server = Process(target=run_echo_server, args=('ipc://ECHO:1', ))
-    consumer = Process(target=run_consumer, args=(
-            'ipc://ECHO:1', 'ipc://CONSUMER_TEST_PUSH_BE:1',
-            'ipc://CONSUMER_TEST_PULL_BE:1', 0.5, ))
-    producer = Process(target=run_producer, args=(
+    echo_server = Process(target=EchoServer('ipc://ECHO:1', ).run)
+    consumer = Process(target=Consumer('ipc://ECHO:1',
+                                       'ipc://CONSUMER_TEST_PUSH_BE:1',
+                                       'ipc://CONSUMER_TEST_PULL_BE:1',
+                                       0.5).run
+    )
+    producer = Process(target=Producer(
             'ipc://PRODUCER_REP:1',
             'ipc://PRODUCER_PUB:1',
             'ipc://CONSUMER_TEST_PULL_BE:1',
-            'ipc://CONSUMER_TEST_PUSH_BE:1',
-    ))
-    client = Process(target=run_client, args=('ipc://PRODUCER_REP:1',
-                                              'ipc://PRODUCER_PUB:1'))
+            'ipc://CONSUMER_TEST_PUSH_BE:1').run
+    )
+    client = Process(target=Client('ipc://PRODUCER_REP:1',
+                                   'ipc://PRODUCER_PUB:1').run)
 
     try:
         echo_server.start()
